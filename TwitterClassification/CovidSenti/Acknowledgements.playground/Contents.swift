4ed2f7f37f3ac5f463ecc824d1e7c93618587e23
Ananya Karra <66985689+anyakara@users.noreply.github.com>
2024-01-06 20:04:31 -0800

    Uploading the main project file

---
+++ TwitterClassification/CovidSenti/Acknowledgements.playground/Contents.swift
@@ -0,0 +1,17 @@
+// Acknowledgement page for all of the resources used in the development process of this application:
+
+// Credit to my Udemy Instructor for Angela Yu, teaching at the course iOS 13 and Swift 5 - The Complete iOS App Development Bootcamp with walk-through videos focusing on how to bring advanced concepts, including Natural Language Processing and Machine Learning together, to help computer science students like myself attempt to solve real-world problems.
+// https://www.udemy.com/course/ios-13-app-development-bootcamp/
+
+// Credit to Matt Donnelly for their open-source Git Project, which allowed me to extract Twitter data for analysis:
+// https://github.com/mattdonnelly/Swifter
+
+// Likewise, the article on Natural Language Processing by Jason Brownlee
+// https://machinelearningmastery.com/natural-language-processing/
+
+// Another Interesting Read on Sentiment Analysis:
+// https://www.visualcapitalist.com/word-sentiment-scale/
+
+
+
+
